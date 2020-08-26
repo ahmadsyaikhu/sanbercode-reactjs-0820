a7321fe400084cbb745ad9efc4ee81b145c0724c
ahmadsyaikhu <ahmadsyaikhu99@gmail.com>
2020-08-27 00:50:28 +0700

    update tugas-3

--- Tugas-3/tugas3.js
+++ Tugas-3/tugas3.js
@@ -1,54 +1,90 @@
-// soal 1
-var kataPertama = "saya" ;
-var kataKedua   = "senang" ;
-var kataKetiga  = "belajar" ;
-var katakeempat = "javascript";
-// ....jawaban soal 1
+// // soal 1
+// var kataPertama = "saya";
+// var kataKedua   = "senang";
+// var kataKetiga  = "belajar";
+// var kataKeempat = "javascript";
+// // ....jawaban soal 1
+// var kataPertama = "saya";
+// var kataKedua   = "senang";
+// var kataKetiga  = "belajar";
+// var kataKeempat = "javascript";
+// var upper       = kataKeempat.toUpperCase();
+
+// var kalimat     = kataPertama + " " + kataKedua + " "+ kataKetiga + upper ;
+// console.log(kalimat);
 
 
 
 
+// // soal 2
+// var kataPertama = "1";
+// var kataKedua   = "2";
+// var kataKetiga  = "4";
+// var kataKeempat = "5";
+// // ....jawaban soal 2
+// var kataPertama = Number("1");
+// var kataKedua   = Number("2");
+// var kataKetiga  = Number("4");
+// var kataKeempat = Number("5");
 
-// soal 2
-var kataPertama = "1" ;
-var kataKedua   = "2" ;
-var kataKetiga  = "3" ;
-var katakeempat = "5" ;
-// ....jawaban soal 2
+// var hasil       = kataPertama + kataKedua + kataKetiga + kataKeempat;
+// console.log(hasil);
 
+// // soal 3
+// var kalimat = 'wah javascript itu keren sekali'; 
 
-// soal 3
-var kalimat = 'wah javascript itu keren sekali'; 
+// var kataPertama = kalimat.substring(0, 3); 
+// var kataKedua; // do your own! 
+// var kataKetiga; // do your own! 
+// var kataKeempat; // do your own! 
+// var kataKelima; // do your own! 
 
-var kataPertama = kalimat.substring(0, 3); 
-var kataKedua; // do your own! 
-var kataKetiga; // do your own! 
-var kataKeempat; // do your own! 
-var kataKelima; // do your own! 
-
-console.log('Kata Pertama: ' + kataPertama); 
-console.log('Kata Kedua: ' + kataKedua); 
-console.log('Kata Ketiga: ' + kataKetiga); 
-console.log('Kata Keempat: ' + kataKeempat); 
-console.log('Kata Kelima: ' + kataKelima);
-// ....jawaban soal 3
+// // console.log('Kata Pertama: ' + kataPertama); 
+// // console.log('Kata Kedua: ' + kataKedua); 
+// // console.log('Kata Ketiga: ' + kataKetiga); 
+// // console.log('Kata Keempat: ' + kataKeempat); 
+// // console.log('Kata Kelima: ' + kataKelima);
+// // ....jawaban soal 3
 
 // soal 4
+// nilai >= 80 indeksnya A
+// nilai >= 70 dan nilai < 80 indeksnya B
+// nilai >= 60 dan nilai < 70 indeksnya c
+// nilai >= 50 dan nilai < 60 indeksnya D
+// nilai < 50 indeksnya E
+// // ....jawaban soal 4
 
-var nilai;
-nilai >= 80 indeksnya A
-nilai >= 70 dan nilai < 80 indeksnya B
-nilai >= 60 dan nilai < 70 indeksnya c
-nilai >= 50 dan nilai < 60 indeksnya D
-nilai < 50 indeksnya E
-// ....jawaban soal 4
+var nilai = 85;
+
+if        (nilai >= 80){
+    console.log('index A');
+} else if (nilai >= 70){
+    console.log('index B'); 
+} else if (nilai >= 60){
+    console.log('index C'); 
+} else if (nilai >= 50){
+    console.log('index D'); 
+} else {
+    console.log('index E')
+}
 
 
-// soal 5
+// // soal 5
+// var tanggal = 25;
+// var b       = 6;
+// var bulan   = b.setMonth(11);
+// var tahun   = 1990;
+// // ....jawaban soal 3
+// var tanggal = String(25);
 
-var tanggal = 22;
-var bulan = 7;
-var tahun = 2020;
-// ....jawaban soal 5
+// var tahun   = String(1990);
+
+// console.log(bulan);
+
+
+
+
+
+
 
 
