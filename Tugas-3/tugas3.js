edcae930ba30e56cde7fd719fc7838a7f7d646ff
ahmadsyaikhu <ahmadsyaikhu99@gmail.com>
2020-08-26 21:26:29 +0700

    soal tugas-3

---
+++ Tugas-3/tugas3.js
@@ -0,0 +1,52 @@
+// soal 1
+var kataPertama = "saya" ;
+var kataKedua   = "senang" ;
+var kataKetiga  = "belajar" ;
+var katakeempat = "javascript";
+// ....jawaban soal 1
+
+
+
+
+
+// soal 2
+var kataPertama = "1" ;
+var kataKedua   = "2" ;
+var kataKetiga  = "3" ;
+var katakeempat = "5" ;
+// ....jawaban soal 2
+
+
+// soal 3
+var kalimat = 'wah javascript itu keren sekali'; 
+
+var kataPertama = kalimat.substring(0, 3); 
+var kataKedua; // do your own! 
+var kataKetiga; // do your own! 
+var kataKeempat; // do your own! 
+var kataKelima; // do your own! 
+
+console.log('Kata Pertama: ' + kataPertama); 
+console.log('Kata Kedua: ' + kataKedua); 
+console.log('Kata Ketiga: ' + kataKetiga); 
+console.log('Kata Keempat: ' + kataKeempat); 
+console.log('Kata Kelima: ' + kataKelima);
+// ....jawaban soal 3
+
+// soal 4
+
+var nilai;
+nilai >= 80 indeksnya A
+nilai >= 70 dan nilai < 80 indeksnya B
+nilai >= 60 dan nilai < 70 indeksnya c
+nilai >= 50 dan nilai < 60 indeksnya D
+nilai < 50 indeksnya E
+// ....jawaban soal 4
+
+
+// soal 5
+
+var tanggal = 22;
+var bulan = 7;
+var tahun = 2020;
+// ....jawaban soal 5
