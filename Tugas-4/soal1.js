f79bd6f05251a876d801d228ebac63ec1033b431
ahmadsyaikhu <ahmadsyaikhu99@gmail.com>
2020-08-27 23:35:43 +0700

    update tugas-4

---
+++ Tugas-4/soal1.js
@@ -0,0 +1,57 @@
+
+// Soal 1
+// console.log("LOOPING PERTAMA")
+
+// var nilai = 2;
+// while ( nilai <= 20) {
+//     console.log(nilai + ' -  I Love coding  ');
+//     nilai+=2;
+// }
+
+// console.log("LOOPING KEDUA")
+
+// var nilai = 20;
+// while ( nilai >= 2) {
+//     console.log(nilai + ' -  I will become a frontend developer  ');
+//     nilai-=2;
+// }
+
+
+// Soal 2
+
+// for(var nilai = 0; nilai <= 20; nilai++){
+    
+//     if (nilai % 2 == 0 ) {
+//         console.log('Santai');
+//     } else if ( nilai ===  3  && nilai % 2 != 0 ) {
+//         console.log('I Love Coding')
+//     }    else {
+//         console.log('Berkualitas');
+//     }
+// }
+
+// Soal 3
+
+// for (var i = 0; i <= 7; i++) {
+//     for ( var j = 1; j <= i; j++){
+//         console.log('#' + '')
+//     }
+//         console.log(' ')
+// }
+
+// Soal 4
+
+// var kalimat= "saya sangat senang belajar javascript"
+
+// kalimat.split(' ');
+
+// console.log(kalimat);
+
+
+// Soal 5
+
+var daftarBuah = ["2. Apel", "5. Jeruk", "3. Anggur", "4. Semangka", "1. Mangga"];
+
+daftarBuah.sort();
+
+console.log(daftarBuah);
